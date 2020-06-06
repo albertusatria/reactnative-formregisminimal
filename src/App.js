53ae5bf683ae32630d861b268f8d364f0308cec8
albertusatria <albert@icube.us>
2020-06-06 15:13:59 +0700

    code refactor

--- src/App.js
+++ src/App.js
@@ -13,10 +13,7 @@ import FirstLast from './component/FirstLast';
 import ButtonCustom from './component/ButtonCustom';
 
 const App = () => {
-  const [firstname, setFirstname] = useState(Platform.OS === 'ios' ? '' : null);
-  const [lastname, setLastname] = useState(Platform.OS === 'ios' ? '' : null);
   const callbackFirstLastName = (value) => {
-    setFirstname(value.firstname);
     console.log(value.lastname);
   };
   return (
@@ -26,7 +23,6 @@ const App = () => {
           <ScrollView>
             <FirstLast callbackFirstLastName={callbackFirstLastName} />
             <ButtonCustom type="Register" />
-            <Text>{firstname}</Text>
           </ScrollView>
         </View>
       </SafeAreaView>

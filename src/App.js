a4a7b9d88dce5dced9ca32fa670f363af11f6905
albertusatria <albert@icube.us>
2020-06-13 13:22:40 +0700

    navigation tab simple implementation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// import {createStackNavigator} from '@react-navigation/stack';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+// const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      {/*<Stack.Navigator>
         <Stack.Screen
           name="Home"
           component={Home}
           options={{title: 'Welcome'}}
         />
         <Stack.Screen name="Profile" component={Profile} />
-      </Stack.Navigator>
+      </Stack.Navigator>*/}
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };

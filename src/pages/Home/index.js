7256692231b2ed108028a773f8a68bfbdae188d1
albertusatria <albert@icube.us>
2020-06-13 14:56:56 +0700

    adding simple gql mutation request

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import {Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  SafeAreaView,
+  ScrollView,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
+import stylesLogin from '../../asset/style';
+import {gql} from 'apollo-boost';
+import {mutate} from '../../services/graphql/api';
 
 const Home = ({navigation}) => {
+  const [username, setUsername] = useState(Platform.OS === 'ios' ? '' : null);
+  const [password, setPassword] = useState(Platform.OS === 'ios' ? '' : null);
+  const postLogin = () => {
+    console.log('postLogin');
+    let schema = gql`
+      mutation generateCustomerTokenCustom(
+        $email: String!
+        $password: String!
+      ) {
+        generateCustomerTokenCustom(username: $email, password: $password) {
+          token
+        }
+      }
+    `;
+    let querySent = {email: 'tomo@icube.us', password: 'Admin123'};
+    //let querySent = {email: username, password: password};
+    console.log(username);
+    mutate(schema, querySent).then((response) => {
+      const {data} = response;
+      const userData = data.generateCustomerTokenCustom;
+      console.log(userData.token);
+    });
+  };
   return (
-    <Button
-      title="Go to John's profile"
-      onPress={() => navigation.navigate('Profile', {name: 'John'})}
-    />
+    <View>
+      <SafeAreaView>
+        <View>
+          <ScrollView>
+            <View style={stylesLogin.wrapper}>
+              <View style={stylesLogin.fieldControl}>
+                <Text style={stylesLogin.label}>Username</Text>
+                <TextInput onChange={(text) => setUsername(text)} />
+              </View>
+              <View style={stylesLogin.fieldControl}>
+                <Text style={stylesLogin.label}>Password</Text>
+                <TextInput onChange={(text) => setPassword(text)} />
+              </View>
+              <View>
+                <TouchableOpacity
+                  style={stylesLogin.btnSubmit}
+                  onPress={postLogin}>
+                  <Text style={stylesLogin.btnText}>Login</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Button, View, Text} from 'react-native';
 
 const Profile = ({navigation}) => {
-  console.log(navigation);
+  //   console.log(navigation);
   return (
     <View>
       <Text>Profile Page</Text>

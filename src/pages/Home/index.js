bcda4372144a8dd122bbb05325a0ff685c8338b2
albertusatria <albert@icube.us>
2020-06-13 15:16:43 +0700

    adding localStorage simple value - fixing onChangeText

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,10 +11,22 @@ import {
 import stylesLogin from '../../asset/style';
 import {gql} from 'apollo-boost';
 import {mutate} from '../../services/graphql/api';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Home = ({navigation}) => {
   const [username, setUsername] = useState(Platform.OS === 'ios' ? '' : null);
   const [password, setPassword] = useState(Platform.OS === 'ios' ? '' : null);
+  const storeData = async (value) => {
+    try {
+      await AsyncStorage.setItem('token', value);
+    } catch (e) {
+      // saving error
+    }
+  };
+  const checkLocal = async () => {
+    const value = await AsyncStorage.getItem('token');
+    console.log(value);
+  };
   const postLogin = () => {
     console.log('postLogin');
     let schema = gql`
@@ -27,13 +39,13 @@ const Home = ({navigation}) => {
         }
       }
     `;
-    let querySent = {email: 'tomo@icube.us', password: 'Admin123'};
-    //let querySent = {email: username, password: password};
-    console.log(username);
+    let querySent = {email: username, password: password};
+    // console.log(username);
     mutate(schema, querySent).then((response) => {
       const {data} = response;
       const userData = data.generateCustomerTokenCustom;
-      console.log(userData.token);
+      //   console.log(userData.token);
+      storeData(userData.token);
     });
   };
   return (
@@ -44,17 +56,30 @@ const Home = ({navigation}) => {
             <View style={stylesLogin.wrapper}>
               <View style={stylesLogin.fieldControl}>
                 <Text style={stylesLogin.label}>Username</Text>
-                <TextInput onChange={(text) => setUsername(text)} />
+                <TextInput
+                  style={stylesLogin.input}
+                  onChangeText={(text) => setUsername(text)}
+                />
               </View>
               <View style={stylesLogin.fieldControl}>
                 <Text style={stylesLogin.label}>Password</Text>
-                <TextInput onChange={(text) => setPassword(text)} />
+                <TextInput
+                  style={stylesLogin.input}
+                  onChangeText={(text) => setPassword(text)}
+                />
               </View>
               <View>
                 <TouchableOpacity
                   style={stylesLogin.btnSubmit}
                   onPress={postLogin}>
                   <Text style={stylesLogin.btnText}>Login</Text>
+                </TouchableOpacity>
+              </View>
+              <View>
+                <TouchableOpacity
+                  style={stylesLogin.btnSubmit}
+                  onPress={checkLocal}>
+                  <Text style={stylesLogin.btnText}>Check Local Storage</Text>
                 </TouchableOpacity>
               </View>
             </View>

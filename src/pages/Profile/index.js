65c7b273f41ba9c2b1a3b86e7dc14298216a7655
albertusatria <albert@icube.us>
2020-06-13 11:46:08 +0700

    adding simple back button

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Button, View, Text} from 'react-native';
 
 const Profile = ({navigation}) => {
   console.log(navigation);
   return (
     <View>
       <Text>Profile Page</Text>
+      <Button title="<< Back" onPress={() => navigation.goBack()} />
     </View>
   );
 };
